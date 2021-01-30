1b074dd012134c90702d1f5a0b02821937cf3d70
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-01-30 14:57:32 +0700

    Update DevBox-Windows2012.sh

--- DevBox-Windows2012.sh
+++ DevBox-Windows2012.sh
@@ -38,7 +38,7 @@ echo All done! Connect your VM using RDP.
 echo IP:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo User: Administrator
-echo Pass: rioHACKER123
+echo Pass: Thuonghai001
 echo Finished! You can Close PUTTY now.
 
 
